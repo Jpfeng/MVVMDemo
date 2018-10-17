24f1c16ddc83d083bfbe408382460fa0b110805f
Jpfeng <fengjp@mixotc.com>
2018-10-17 11:05:27 +0800

    clean up

--- app/src/main/java/com/jpfeng/mvvmdemo/MainActivity.java
+++ app/src/main/java/com/jpfeng/mvvmdemo/MainActivity.java
@@ -1,16 +1,14 @@
 package com.jpfeng.mvvmdemo;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MediatorLiveData;
 import android.arch.lifecycle.MutableLiveData;
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,21 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mModel = ViewModelProviders.of(this).get(MainViewModel.class);
         final ViewDataBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
-        mModel.getText1().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                binding.setVariable(BR.text1, s);
-            }
-        });
-
-        mModel.getText2().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                binding.setVariable(BR.text2, s);
-            }
-        });
-
+        mModel.getText1().observe(this, s -> binding.setVariable(BR.text1, s));
+        mModel.getText2().observe(this, s -> binding.setVariable(BR.text2, s));
         binding.setVariable(BR.handler, new ClickHandler());
     }
 
@@ -46,19 +31,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static class MainViewModel extends ViewModel {
-        private final MutableLiveData<String> text1 = new MutableLiveData<>();
-        private final MutableLiveData<String> text2 = new MutableLiveData<>();
+        private final MediatorLiveData<String> text1 = new MediatorLiveData<>();
+        private final MediatorLiveData<String> text2 = new MediatorLiveData<>();
         private final MutableLiveData<Integer> count = new MutableLiveData<>();
 
         public MainViewModel() {
-            count.observeForever(new Observer<Integer>() {
-                @Override
-                public void onChanged(@Nullable Integer integer) {
-                    text1.setValue("String " + integer);
-                    text2.setValue("Text " + integer);
-                }
-            });
-
+            text1.addSource(count, integer -> text1.setValue("String " + integer));
+            text2.addSource(count, integer -> text2.setValue("Text " + integer));
             count.setValue(0);
         }
 
